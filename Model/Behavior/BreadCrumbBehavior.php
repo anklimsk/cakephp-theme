6472280fe7b848dfd1805596d958a68517376355
Andrey Klimov <anklimsk@gmail.com>
2018-09-28 10:49:29 +0300

    Convert code to CakePHP Coding Standards

--- Model/Behavior/BreadCrumbBehavior.php
+++ Model/Behavior/BreadCrumbBehavior.php
@@ -17,229 +17,218 @@ App::uses('CakeText', 'Utility');
  *
  * @package plugin.Model.Behavior
  */
-class BreadCrumbBehavior extends ModelBehavior
-{
+class BreadCrumbBehavior extends ModelBehavior {
 
-    /**
-     * Return name of model that uses this behavior.
-     *
-     * @param Model $model Model using this behavior.
-     * @param bool $toLowerCase If True, return the model name in lowercase.
-     * @return string Return name of model that uses this behavior.
-     */
-    public function getModelName(Model $model, $toLowerCase = false)
-    {
-        $modelName = $model->name;
-        if ($toLowerCase) {
-            $modelName = mb_strtolower($modelName);
-        }
+/**
+ * Return name of model that uses this behavior.
+ *
+ * @param Model $model Model using this behavior.
+ * @param bool $toLowerCase If True, return the model name in lowercase.
+ * @return string Return name of model that uses this behavior.
+ */
+	public function getModelName(Model $model, $toLowerCase = false) {
+		$modelName = $model->name;
+		if ($toLowerCase) {
+			$modelName = mb_strtolower($modelName);
+		}
 
-        return $modelName;
-    }
+		return $modelName;
+	}
 
-    /**
-     * Return plural name of model that uses this behavior.
-     *
-     * @param Model $model Model using this behavior.
-     * @param bool $toLowerCase If True, return the model name in lowercase.
-     * @return string Return plural name of model that uses this behavior.
-     */
-    public function getModelNamePlural(Model $model, $toLowerCase = false)
-    {
-        $modelName = $this->getModelName($model, $toLowerCase);
+/**
+ * Return plural name of model that uses this behavior.
+ *
+ * @param Model $model Model using this behavior.
+ * @param bool $toLowerCase If True, return the model name in lowercase.
+ * @return string Return plural name of model that uses this behavior.
+ */
+	public function getModelNamePlural(Model $model, $toLowerCase = false) {
+		$modelName = $this->getModelName($model, $toLowerCase);
 
-        return Inflector::pluralize($modelName);
-    }
+		return Inflector::pluralize($modelName);
+	}
 
-    /**
-     * Return plugin name.
-     *
-     * @param Model $model Model using this behavior.
-     * @return string Return plugin name for breadcrumb.
-     */
-    public function getPluginName(Model $model)
-    {
-        $pluginName = null;
+/**
+ * Return plugin name.
+ *
+ * @param Model $model Model using this behavior.
+ * @return string Return plugin name for breadcrumb.
+ */
+	public function getPluginName(Model $model) {
+		$pluginName = null;
 
-        return $pluginName;
-    }
+		return $pluginName;
+	}
 
-    /**
-     * Return controller name.
-     *
-     * @param Model $model Model using this behavior.
-     * @return string Return controller name for breadcrumb.
-     */
-    public function getControllerName(Model $model)
-    {
-        $modelNamePlural = $this->getModelNamePlural($model, false);
-        $controllerName = mb_strtolower(Inflector::underscore($modelNamePlural));
+/**
+ * Return controller name.
+ *
+ * @param Model $model Model using this behavior.
+ * @return string Return controller name for breadcrumb.
+ */
+	public function getControllerName(Model $model) {
+		$modelNamePlural = $this->getModelNamePlural($model, false);
+		$controllerName = mb_strtolower(Inflector::underscore($modelNamePlural));
 
-        return $controllerName;
-    }
+		return $controllerName;
+	}
 
-    /**
-     * Return action name.
-     *
-     * @param Model $model Model using this behavior.
-     * @return string Return action name for breadcrumb.
-     */
-    public function getActionName(Model $model)
-    {
-        $actionName = 'view';
+/**
+ * Return action name.
+ *
+ * @param Model $model Model using this behavior.
+ * @return string Return action name for breadcrumb.
+ */
+	public function getActionName(Model $model) {
+		$actionName = 'view';
 
-        return $actionName;
-    }
+		return $actionName;
+	}
 
-    /**
-     * Return name of group data.
-     *
-     * @param Model $model Model using this behavior.
-     * @return string Return name of group data
-     */
-    public function getGroupName(Model $model)
-    {
-        $modelNamePlural = $this->getModelNamePlural($model);
-        $groupNameCamel = Inflector::humanize(Inflector::underscore($modelNamePlural));
-        $groupName = mb_ucfirst(mb_strtolower($groupNameCamel));
+/**
+ * Return name of group data.
+ *
+ * @param Model $model Model using this behavior.
+ * @return string Return name of group data
+ */
+	public function getGroupName(Model $model) {
+		$modelNamePlural = $this->getModelNamePlural($model);
+		$groupNameCamel = Inflector::humanize(Inflector::underscore($modelNamePlural));
+		$groupName = mb_ucfirst(mb_strtolower($groupNameCamel));
 
-        return $groupName;
-    }
+		return $groupName;
+	}
 
-    /**
-     * Return name of data.
-     *
-     * @param Model $model Model using this behavior.
-     * @param int|string|array $id ID of record or array data
-     *  for retrieving name.
-     * @return string|bool Return name of data,
-     *  or False on failure.
-     */
-    public function getName(Model $model, $id = null)
-    {
-        if (empty($id) || empty($model->displayField)) {
-            return false;
-        }
-        if (is_array($id)) {
-            if (!isset($id[$model->alias][$model->displayField])) {
-                return false;
-            }
+/**
+ * Return name of data.
+ *
+ * @param Model $model Model using this behavior.
+ * @param int|string|array $id ID of record or array data
+ *  for retrieving name.
+ * @return string|bool Return name of data,
+ *  or False on failure.
+ */
+	public function getName(Model $model, $id = null) {
+		if (empty($id) || empty($model->displayField)) {
+			return false;
+		}
+		if (is_array($id)) {
+			if (!isset($id[$model->alias][$model->displayField])) {
+				return false;
+			}
 
-            $result = $id[$model->alias][$model->displayField];
-        } else {
-            $model->id = $id;
-            $result = $model->field($model->displayField);
-        }
+			$result = $id[$model->alias][$model->displayField];
+		} else {
+			$model->id = $id;
+			$result = $model->field($model->displayField);
+		}
 
-        return $result;
-    }
+		return $result;
+	}
 
-    /**
-     * Return full name of data.
-     *
-     * @param Model $model Model using this behavior.
-     * @param int|string|array $id ID of record or array data
-     *  for retrieving full name.
-     * @return string|bool Return full name of data,
-     *  or False on failure.
-     */
-    public function getFullName(Model $model, $id = null)
-    {
-        $name = $this->getName($model, $id);
-        if (empty($name)) {
-            return false;
-        }
+/**
+ * Return full name of data.
+ *
+ * @param Model $model Model using this behavior.
+ * @param int|string|array $id ID of record or array data
+ *  for retrieving full name.
+ * @return string|bool Return full name of data,
+ *  or False on failure.
+ */
+	public function getFullName(Model $model, $id = null) {
+		$name = $this->getName($model, $id);
+		if (empty($name)) {
+			return false;
+		}
 
-        $modelName = $this->getModelName($model);
-        $result = $modelName . ' \'' . $name . '\'';
+		$modelName = $this->getModelName($model);
+		$result = $modelName . ' \'' . $name . '\'';
 
-        return $result;
-    }
+		return $result;
+	}
 
-    /**
-     * Return an array of information for creating a breadcrumb.
-     *
-     * @param Model $model Model using this behavior.
-     * @param int|string|array $id ID of record or array data
-     *  for retrieving name.
-     * @param array|bool|null $link URL for breadcrumb or False to disable auto creation.
-     * @return array Return an array of information for creating a breadcrumb.
-     */
-    public function createBreadcrumb(Model $model, $id = null, $link = null)
-    {
-        $result = [];
-        if (empty($id)) {
-            $name = $model->getGroupName();
-        } else {
-            $name = $model->getName($id);
-        }
-        if (empty($name)) {
-            return $result;
-        }
+/**
+ * Return an array of information for creating a breadcrumb.
+ *
+ * @param Model $model Model using this behavior.
+ * @param int|string|array $id ID of record or array data
+ *  for retrieving name.
+ * @param array|bool|null $link URL for breadcrumb or False to disable auto creation.
+ * @return array Return an array of information for creating a breadcrumb.
+ */
+	public function createBreadcrumb(Model $model, $id = null, $link = null) {
+		$result = [];
+		if (empty($id)) {
+			$name = $model->getGroupName();
+		} else {
+			$name = $model->getName($id);
+		}
+		if (empty($name)) {
+			return $result;
+		}
 
-        if ($link === false) {
-            $link = null;
-        } else {
-            if (!is_array($link)) {
-                $link = [];
-            }
-            $plugin = $model->getPluginName();
-            $controller = $model->getControllerName();
-            if (empty($id)) {
-                $action = 'index';
-            } else {
-                $action = $model->getActionName();
-                if (empty($link)) {
-                    $link[] = $id;
-                }
-            }
-            $link += compact('plugin', 'controller', 'action');
-        }
+		if ($link === false) {
+			$link = null;
+		} else {
+			if (!is_array($link)) {
+				$link = [];
+			}
+			$plugin = $model->getPluginName();
+			$controller = $model->getControllerName();
+			if (empty($id)) {
+				$action = 'index';
+			} else {
+				$action = $model->getActionName();
+				if (empty($link)) {
+					$link[] = $id;
+				}
+			}
+			$link += compact('plugin', 'controller', 'action');
+		}
 
-        $name = CakeText::truncate(h($name), 20);
-        $result = [
-            $name,
-            $link
-        ];
+		$name = CakeText::truncate(h($name), 20);
+		$result = [
+			$name,
+			$link
+		];
 
-        return $result;
-    }
+		return $result;
+	}
 
-    /**
-     * Return an array of information for creating a breadcrumbs.
-     *
-     * @param Model $model Model using this behavior.
-     * @param int|string|array $id ID of record or array data
-     *  for retrieving name.
-     * @param bool|null $includeRoot If True, include information of root breadcrumb.
-     *  If Null, include information of root breadcrumb if $ID is not empty.
-     * @return array Return an array of information for creating a breadcrumbs.
-     */
-    public function getBreadcrumbInfo(Model $model, $id = null, $includeRoot = null)
-    {
-        if (!empty($id) && ($includeRoot === null)) {
-            $includeRoot = true;
-        }
+/**
+ * Return an array of information for creating a breadcrumbs.
+ *
+ * @param Model $model Model using this behavior.
+ * @param int|string|array $id ID of record or array data
+ *  for retrieving name.
+ * @param bool|null $includeRoot If True, include information of root breadcrumb.
+ *  If Null, include information of root breadcrumb if $ID is not empty.
+ * @return array Return an array of information for creating a breadcrumbs.
+ */
+	public function getBreadcrumbInfo(Model $model, $id = null, $includeRoot = null) {
+		if (!empty($id) && ($includeRoot === null)) {
+			$includeRoot = true;
+		}
 
-        $result = [];
-        $root = [];
-        if ($includeRoot) {
-            $root = $model->getBreadcrumbInfo();
-            if (empty($root)) {
-                return $result;
-            }
-        }
+		$result = [];
+		$root = [];
+		if ($includeRoot) {
+			$root = $model->getBreadcrumbInfo();
+			if (empty($root)) {
+				return $result;
+			}
+		}
 
-        $info = $model->createBreadcrumb($id);
-        if (empty($info)) {
-            return $result;
-        }
+		$info = $model->createBreadcrumb($id);
+		if (empty($info)) {
+			return $result;
+		}
 
-        if (!empty($root)) {
-            $result = $root;
-        }
-        $result[] = $info;
+		if (!empty($root)) {
+			$result = $root;
+		}
+		$result[] = $info;
 
-        return $result;
-    }
+		return $result;
+	}
 }
